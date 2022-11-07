2f2ffe18c38ac10382ce91174ec617d91864dea6
user <user@users-MacBook-Pro-2.local>
2022-11-07 00:12:41 -0700

    feat: add endpoint for ethlint

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config.js');
+const Solium = require('solium');
+const { readFile } = require('fs/promises')
+
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
 const app = express();
+
+
 
 if (isDeveloping) {
   const compiler = webpack(config);
@@ -28,15 +33,12 @@ if (isDeveloping) {
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-    res.end();
-  });
+
+
+
 } else {
   app.use(express.static(__dirname + '/dist'));
-  app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
+
 }
 
 app.listen(port, '0.0.0.0', function onStart(err) {
@@ -44,4 +46,32 @@ app.listen(port, '0.0.0.0', function onStart(err) {
     console.log(err);
   }
   console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+  
+});
+
+app.get('/test', async function response(req, res) {
+  async function content(path) {  
+    return await readFile(path, 'utf8')
+  }
+  const text = await content('./contracts/Migrations.sol');  
+
+  
+        sourceCode = text;
+
+const errors = Solium.lint(sourceCode, {
+        "extends": "solium:recommended",
+        "plugins": ["security"],
+        "rules": {
+                "quotes": ["error", "double"],
+                "double-quotes": [2],   // returns a rule deprecation warning
+                "pragma-on-top": 1
+        },
+
+        "options": { "returnInternalIssues": true }
+});
+
+errors.forEach(console.log);
+res.json({
+  errors: errors
+})
 });
