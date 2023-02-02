9c46fb8d1d35d10c43615d2a22a9418689ae05ed
user <user@users-MacBook-Pro-2.local>
2023-02-01 22:28:56 -0700

    add referrer policy header

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.listen(port, '0.0.0.0', function onStart(err) {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Referrer-Policy", "no-referrer");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
