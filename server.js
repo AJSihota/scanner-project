2972251c2de167b43a5f3fb51ed0d81d7f3b315d
user <user@users-MacBook-Pro-2.local>
2023-02-02 19:03:19 -0700

    add headers for CORS

--- server.js
+++ server.js
@@ -58,9 +58,19 @@ app.use(function(req, res, next) {
 });
 
 app.post('/upload', jsonParser, async function response(req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-res.header("Referrer-Policy", "no-referrer");
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
   async function content(path) {  
     return await readFile(path, 'utf8')
   }
